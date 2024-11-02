808ad80496928fe078e944f0074ef28aeb6d4ff7
alextaylor04 <at1587kc@gmail.com>
2024-11-02 02:50:38 -0500

    Partially Broken Google Maps, Fixed Cursor, Custom Button Added But Not Working.

--- agriculture-front-end/src/MapComp.js
+++ agriculture-front-end/src/MapComp.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {useEffect, useState } from 'react';
 import { GoogleMap, LoadScript, Marker, useLoadScript } from '@react-google-maps/api';
 import raw from './passwords.txt';
+import { Button } from 'bootstrap';
 
 const MapComp = () => {
   const [isVisible, setIsVisible] = useState(false);
   const [stringKey, setKey] = useState('');
+  const [map, setMap] = useState(null);
+  const [showLabels, setShowLabels] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -20,11 +24,14 @@ const MapComp = () => {
     setKey(text);
   });
   const options = {
-    disableDefaultUI: false,
+    disableDefaultUI: true,
     mapTypeId: 'satellite',
-    scaleControl: false,
+    scaleControl: true,
     streetViewControl: false,
-    fullscreenControl: true
+    mapTypeControl: true,
+    fullscreenControl: true,
+    draggableCursor: 'default',
+    styles: showLabels ? [] : [{ featureType: 'all', elementType: 'labels', stylers: [{ visibility: 'off' }] }],
   };
 
   const center = {
@@ -37,6 +44,63 @@ const MapComp = () => {
     console.log("lat:" + lat);
     console.log("lng:" + lng);
   }
+const CustomControlButton = ({ onClick }) => {
+  return (
+    <button
+      style={{
+        backgroundColor: '#fff',
+        border: '2px solid #000',
+        borderRadius: '3px',
+        padding: '5px',
+        cursor: 'pointer',
+        margin: '10px',
+      }}
+      onClick={onClick}
+    >
+      Click me
+    </button>
+  );
+};
+
+const handleButtonClick = () => {
+  setShowLabels((prev) => !prev); // Toggle the state
+
+    if (map) {
+      // Update the styles based on the new state
+      map.setOptions({
+        styles: !showLabels ? [] : [{ featureType: 'all', elementType: 'labels', stylers: [{ visibility: 'off' }] }],
+      });
+    }
+};
+
+const LoadControl = function(map) {
+  if (map) {
+    const controlDiv = document.createElement('div');
+
+    const buttonElement = ReactDOM.render(
+      <CustomControlButton onClick={handleButtonClick} />,
+      controlDiv
+    );
+
+    const root = ReactDOM.createRoot(controlDiv);
+    root.render(<CustomControlButton onClick={handleButtonClick} />);
+
+    map.controls[window.google.maps.ControlPosition.TOP_CENTER].push(controlDiv);
+  }
+}
+useEffect(() => {
+  if (map) {
+    LoadControl(map); // Add custom control when the map instance is set
+    // Update the map options to reflect the current state of showLabels
+    map.setOptions({
+      styles: showLabels
+        ? [] 
+        : [{ featureType: 'all', elementType: 'labels', stylers: [{ visibility: 'off' }] }],
+    });
+  }
+}, [map, showLabels]); // Run this effect when map or showLabels changes
+
+
     return (
       <div>
       {isVisible ? (
@@ -50,6 +114,10 @@ const MapComp = () => {
             center={center}
             options={options}
             zoom={15}
+            onLoad={(mapInstance) => {
+              setMap(mapInstance);
+              LoadControl(mapInstance); 
+            }}
             onClick= {ev => {
               Mapclick(ev)
             }}
@@ -66,3 +134,4 @@ const MapComp = () => {
 };
 
 export default MapComp;
+
